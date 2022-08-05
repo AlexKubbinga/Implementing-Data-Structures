956346239c8ef76131e6918de88d4c77f0df9687
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-08-05 16:11:31 +0100

    implemented heaps and heap methods

--- scripts/heap.js
+++ scripts/heap.js
@@ -1,11 +1,123 @@
 'use strict';
 
+// Heaps are usually implemented as arrays
+// Parent:Child relationship
+//given i, its children are at 2i+1 and 2i+2 and its parent is at Math.floor((i-1)/2)
+
+// 2^h -1 gives you the last index of height row
 class Heap {
-  constructor () {
-    this.storage = [];
+  constructor(){
+    this.array = [];
   }
-  add(value) {}
+  insert (value) {
+    console.log(this.array);
+    let index;
+    let nullIndex = this.array.indexOf(null);
+    if (nullIndex !== -1) {
+      this.array[nullIndex] = value;
+      index = nullIndex;
+    } else {
+      this.array.push(value);
+      index = this.array.length -1;
+    }
+    if (this.array.length ===1) return true // if adding root, treat it differently
+    // check parent, if its bigger than parent, swap and continue checking parents.
+    while (value > this.array[Math.floor((index-1)/2)]) {
+      let parent_index = Math.floor((index-1)/2);
+      let temp = this.array[parent_index]
+      this.array[index] = temp;
+      this.array[parent_index] = value;
+      index = parent_index;
+    }
+    console.log(this.array);
+    return true;
+  }
+  remove(value) {
+    console.log(`Trying to remove ${value} from ${this.array}`);
+    let index = this.array.indexOf(value);
+    if (index === -1) return false;
+
+    let leftChild = this.array[(2*index) +1];
+    let rightChild = this.array[(2*index) +2];
+
+
+    let replacementIndex, replacement;
+
+
+    while (leftChild || rightChild) {
+      replacement = Math.max(leftChild, rightChild);
+      replacementIndex = this.array.indexOf(replacement);
+      this.array[index] = replacement;
+
+      leftChild =  this.array[(2*replacementIndex) +1];
+      rightChild = this.array[(2*replacementIndex) +2];
+      index = replacementIndex;
+    }
+
+      this.array[replacementIndex] = null;
+
+      return true;
+  }
+  extract_max () {
+    let root = this.array[0]
+    this.remove(root);
+    return root;
+  }
+
 }
 
 
-module.exports = Set;
+
+
+
+// This failed because to insert a value need to know the index of parent and not possible unless made a global heap array
+// At this point we realized might as well just implement as array anyway and changed course.
+class HeapAttemptFail {
+  constructor (value) {
+    this.root = {value, left:null, right:null};
+  }
+  insert(value) {
+    // two step process of left most insert and swim
+    let checked = leftMostInsert(value);
+
+
+
+
+
+  }
+  extract(value) {}
+
+  replace(value){}
+
+  sink(){}
+
+  swim(){
+
+  }
+  leftMostInsert (value) {
+    let queue = [this.root];
+    let node = {value, left:null, right:null};
+    let checked = [];
+    while (queue.length) {
+      let current = queue.shift()
+      checked.push(current);
+      if (!current.left) {
+        current.left = node;
+        return checked;
+      }
+      else if (!current.right) {
+        current.right = node;
+        return checked;
+      }
+      else{
+        queue.push(current.left)
+        queue.push(current.right)
+      }
+    }
+  }
+
+
+}
+
+
+module.exports = Heap;
--- test/test.js
+++ test/test.js
@@ -4,13 +4,25 @@ var should = require('chai').should();
 
 const Heap = require('../scripts/heap.js');
 
-describe('Set', function () {
+describe('Heap', function () {
 
-  let heap;
+ // Validation that heap works (aka bad tests)
+let heap = new Heap();
+heap.insert(100);
+heap.insert(19);
+heap.insert(36);
+heap.insert(17);
+heap.insert(12);
+heap.insert(20);
+heap.insert(31);
+heap.insert(8);
+heap.insert(9);
+console.log(heap.extract_max());
+heap.insert(45);
+console.log(`Final Heap: [${heap.array}]`);
 
-  beforeEach(function () {
-    heap = new Heap();
-  });
-
+it('Should heapify,insert,remove and extract max ', function () {
+  heap.array.should.eql([45,19,36,17,12,20,31,8,9])
+});
 
 });
