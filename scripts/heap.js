a4e1414ffe8cf1793964f4d7b671a3786a32c935
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-08-05 11:28:32 +0100

    initial commit

---
+++ scripts/heap.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Heap {
+  constructor () {
+    this.storage = [];
+  }
+  add(value) {}
+}
+
+
+module.exports = Set;
---
+++ test/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var should = require('chai').should();
+
+const Heap = require('../scripts/heap.js');
+
+describe('Set', function () {
+
+  let heap;
+
+  beforeEach(function () {
+    heap = new Heap();
+  });
+
+
+});
